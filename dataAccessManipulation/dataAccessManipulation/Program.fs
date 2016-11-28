6be08c3e5e3ce36713a25d66d8ebf31a19cc5286
Maurizio <maurizio.murino@hotmail.com>
2016-11-28 04:17:56 +0100

    Decks 1, 2, 3 ready, health/attack <> 0.

--- dataAccessManipulation/dataAccessManipulation/Program.fs
+++ dataAccessManipulation/dataAccessManipulation/Program.fs
@@ -14,34 +14,37 @@ open FSharp.Data
 open FSharp.Data.JsonExtensions
 
 (*Load data*)
-type Deck = JsonProvider<"https://raw.githubusercontent.com/Maurizio-Mario/Sharpstone/master/SharpStone/deck1.txt">
-let value = Deck.GetSamples()
+type Deck1 = JsonProvider<"https://raw.githubusercontent.com/Maurizio-Mario/Sharpstone/master/SharpStone/deck1.txt">
+let value1 = Deck1.GetSamples()
+
+type Deck2 = JsonProvider<"https://raw.githubusercontent.com/Maurizio-Mario/Sharpstone/master/SharpStone/deck2.txt">
+let value2 = Deck2.GetSamples()
+
+type Deck3 = JsonProvider<"https://raw.githubusercontent.com/Maurizio-Mario/Sharpstone/master/SharpStone/deck3.txt">
+let value3 = Deck3.GetSamples()
 
 // Return every value of the name "Artist" and "Type"
-for col in value do                  //Returns all artists
+for col in value1 do                  //Returns all artists
   if col.Collectible then
     printfn "%s" col.Artist
 
-for ty in value do                   //Retuns how many minions           
+for ty in value1 do                   //Retuns how many minions           
   if ty.Type = "MINION" then
     printfn "%s" ty.Type
 
-value.[1].Type;;                    //Return [1] type
+value1.[1].Type;;                    //Return [1] type
 
-if value.[i].Type = "MINION" then
-    let minDeck1 = value.[i] 
+(*Subset decks with only minions with attack and health <> 0
+The values of health and attack are of type option. See details*)
+let minDeck1 = 
+    value1 |> Seq.filter (fun c -> c.Type = "MINION" && c.Health <> Some 0)
 
-for i in value.[i] do
-    printfn "%s" value
-      
-let rec filter deck key value =
-    match key, value with
-    (key, value) -> 
-         for k in deck do                   //Retuns how many minions           
-            if k.key = "value" then
-                printfn "%s" k.key
-    filter [] " " " "
-                                                
+let minDeck2 =
+    value2 |> Seq.filter (fun c -> c.Type = "MINION" && c.Health <> Some 0)
+
+let minDeck3 =
+    value3 |> Seq.filter (fun c -> c.Type = "MINION" && c.Health <> Some 0)
+                                 
     
 
 
